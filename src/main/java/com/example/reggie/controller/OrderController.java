239b5e7f27581165dc4c6434913300ec615998ca
18427 <1842716801@qq.com>
2022-08-05 13:39:51 +0800

    Redis环境的配置

--- src/main/java/com/example/reggie/controller/OrderController.java
+++ src/main/java/com/example/reggie/controller/OrderController.java
@@ -1,15 +1,17 @@
 package com.example.reggie.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.reggie.common.R;
+import com.example.reggie.entity.Category;
+import com.example.reggie.entity.Employee;
 import com.example.reggie.entity.Orders;
 import com.example.reggie.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 订单
@@ -32,5 +34,50 @@ public class OrderController {
         log.info("订单数据：{}",orders);
         orderService.submit(orders);
         return R.success("下单成功");
+    }
+
+    /***
+     * 显示所有订单
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    public R<Page> page(int page, int pageSize, String number){
+
+        //构造分页器
+        Page pageInfo = new Page(page, pageSize);
+        //构造条件构造器
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper();
+        //添加过滤条件
+        queryWrapper.eq(StringUtils.isNotEmpty(number), Orders::getId,number);
+        //添加排序条件(根据创建时间)
+        queryWrapper.orderByDesc(Orders::getOrderTime);
+        log.info("{}",pageInfo);
+        //执行查询
+        orderService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+
+    }
+    /***
+     * 显示所有订单
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @RequestMapping(value = "/userPage", method = RequestMethod.GET)
+    public R<Page> userPage(int page, int pageSize){
+
+        //构造分页器
+        Page pageInfo = new Page(page, pageSize);
+        //构造条件构造器
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper();
+        //添加排序条件(根据创建时间)
+        queryWrapper.orderByDesc(Orders::getOrderTime);
+        log.info("{}",pageInfo);
+        //执行查询
+        orderService.page(pageInfo, queryWrapper);
+        return R.success(pageInfo);
+
     }
 }
